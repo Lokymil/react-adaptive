db4afc51e47dec876dd3f8b0c4dc213f0f7d3c7e
Lokymil <arrestierchristophe@gmail.com>
2018-10-29 23:26:48 +0100

    Update readme and style

--- README.md
+++ README.md
@@ -1,29 +1,150 @@
-# react-adaptive
+# react-adaptive [![NPM](https://img.shields.io/npm/v/react-adaptive.svg)](https://www.npmjs.com/package/react-adaptive) [![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
 
-> HOC component for adaptive design with React 
+A Higher Order Component (HOC) allowing you to adapt your component display according to its own size and integrate you website with adaptive design.
 
-[![NPM](https://img.shields.io/npm/v/react-adaptive.svg)](https://www.npmjs.com/package/react-adaptive) [![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
+
+## Information
+
+<table>
+<tr>
+<td>Package</td><td>react-adaptive</td>
+</tr>
+<tr>
+<td>Description</td>
+<td>Element size in react for adaptive design</td>
+</tr>
+</table>
 
 ## Install
-
 ```bash
 npm install --save react-adaptive
 ```
 
+
 ## Usage
 
-```jsx
-import React, { Component } from 'react'
+### Using withAdaptive HOC
 
-import MyComponent from 'react-adaptive'
+In that case, you have to define a function receiving the size of the component and returning expected component according to your own process.
 
-class Example extends Component {
-  render () {
+```js
+import React, { Component } from "react";
+import { withAdaptive } from "./ResponsiveComponent";
+
+class LargeComponent extends Component {
+  render() {
     return (
-      <MyComponent />
-    )
+      <div className="flex">
+        My large with HOC and calculation {this.props.name}
+      </div>
+    );
   }
 }
+
+class MediumComponent extends Component {
+  render() {
+    return (
+      <div className="flex">
+        My medium with HOC and calculation {this.props.name}
+      </div>
+    );
+  }
+}
+
+class SmallComponent extends Component {
+  render() {
+    return (
+      <div className="flex">
+        My small with HOC and calculation {this.props.name}
+      </div>
+    );
+  }
+}
+
+const mapSizeToComponent = size => props => {
+  if (size.width < 400) {
+    return <SmallComponent {...props} />;
+  }
+
+  if (size.width < 700) {
+    return <MediumComponent {...props} />;
+  }
+
+  return <LargeComponent {...props} />;
+};
+
+export default withAdaptive({ mapSizeToComponent });
+```
+
+### Using withBreakpoint HOC
+
+In that case, you have to define a set of breakpoints, up to 7, and a set of associated components. You can define an optionnal default Component.  
+Breakpoints name :
+<table>
+  <tr>
+  </tr>
+</table>
+
+```js
+import React, { Component } from "react";
+import { withBreakpoints, breakpoints } from "./ResponsiveComponent";
+
+class LargeComponent extends Component {
+  render() {
+    return (
+      <div className="flex">My large component with HOC {this.props.name}</div>
+    );
+  }
+}
+
+class MediumComponent extends Component {
+  render() {
+    return (
+      <div className="flex">My medium component with HOC {this.props.name}</div>
+    );
+  }
+}
+
+class SmallComponent extends Component {
+  render() {
+    return (
+      <div className="flex">My small component with HOC {this.props.name}</div>
+    );
+  }
+}
+
+const components = {
+  [breakpoints.large]: LargeComponent,
+  [breakpoints.medium]: MediumComponent,
+  [breakpoints.small]: SmallComponent
+};
+
+const breakPoints = {
+  [breakpoints.large]: 1000,
+  [breakpoints.medium]: 700,
+  [breakpoints.small]: 400
+};
+
+export default withBreakpoints(breakPoints)({
+  components,
+  defaultComponent: LargeComponent
+});
+```
+
+### Using withSize HOC
+
+In that case, your component will have an additionnal props as: 
+```js
+size = { width: `current component width` }
+```
+
+```js
+import React from "react";
+import { withSize } from "react-adaptive";
+
+const myComponent = ({ size }) => (<div>My own size is {size.width}</div>);
+
+export default MyComponent;
 ```
 
 ## License
--- src/index.js
+++ src/index.js
@@ -1,95 +1,103 @@
-import React from 'react';
+import React from 'react'
+import classNames from 'classnames'
+import './styles.css'
 
 class ResponsiveWrapper extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			width: 0
-		};
-	}
+  constructor(props) {
+    super(props)
+    this.state = {
+      width: 0
+    }
+  }
 
-	componentDidMount() {
-		this.setState(this.getUpdatedSize());
-		window.addEventListener('resize', this.handleResize);
-	}
+  componentDidMount() {
+    this.setState(this.getUpdatedSize())
+    window.addEventListener('resize', this.handleResize)
+  }
 
-	componentWillUnmount() {
-		if (this.timeOut) {
-			clearTimeout(this.timeOut);
-		}
-		window.removeEventListener('resize', this.handleResize);
-	}
+  componentWillUnmount() {
+    if (this.timeOut) {
+      clearTimeout(this.timeOut)
+    }
+    window.removeEventListener('resize', this.handleResize)
+  }
 
-	getUpdatedSize = () => {
-		if (!this.wrapper) {
-			return null;
-		}
-		return { width: this.wrapper.clientWidth };
-	};
+  getUpdatedSize = () => {
+    if (!this.wrapper) {
+      return null
+    }
+    return { width: this.wrapper.clientWidth }
+  }
 
-	handleResize = () => {
-		if (this.timeOut) {
-			clearTimeout(this.timeOut);
-		}
-		this.timeOut = setTimeout(() => this.setState(this.getUpdatedSize()), 100);
-	};
+  handleResize = () => {
+    if (this.timeOut) {
+      clearTimeout(this.timeOut)
+    }
+    this.timeOut = setTimeout(() => this.setState(this.getUpdatedSize()), 100)
+  }
 
-	render() {
-		return (
-			<div
-				ref={ref => {
-					this.wrapper = ref;
-				}}
-				className={this.props.className}
-			>
-				{this.props.children({ width: this.state.width })}
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div
+        ref={ref => {
+          this.wrapper = ref
+        }}
+				className={classNames("wrapper", this.props.className)}
+      >
+        {this.props.children({ width: this.state.width })}
+      </div>
+    )
+  }
 }
 
-const sizes = {
-	xSmall: 'XSmall',
-	small: 'Small',
-	mediumSmall: 'MediumSmall',
-	medium: 'Medium',
-	mediumLarge: 'MediumLarge',
-	large: 'Large',
-	xLarge: 'XLarge'
-};
+const breakpoints = {
+  xSmall: 'XSmall',
+  small: 'Small',
+  mediumSmall: 'MediumSmall',
+  medium: 'Medium',
+  mediumLarge: 'MediumLarge',
+  large: 'Large',
+  xLarge: 'XLarge'
+}
 
-const withBreakpoints = breakPoints => ({ components, defaultComponent }) => props => {
-	const getSizedElement = size => {
-		let DisplayedComponent = defaultComponent || null;
+const withBreakpoints = componentBreakPoints => ({ components, defaultComponent }) => props => {
+  const getSizedElement = size => {
+    let DisplayedComponent = defaultComponent || null
 
-		if (breakPoints[sizes.xSmall] && breakPoints[sizes.xSmall] >= size.width) {
-			DisplayedComponent = components[sizes.xSmall];
-		} else if (breakPoints[sizes.small] && breakPoints[sizes.small] >= size.width) {
-			DisplayedComponent = components[sizes.small];
-		} else if (breakPoints[sizes.mediumSmall] && breakPoints[sizes.mediumSmall] >= size.width) {
-			DisplayedComponent = components[sizes.mediumSmall];
-		} else if (breakPoints[sizes.medium] && breakPoints[sizes.medium] >= size.width) {
-			DisplayedComponent = components[sizes.medium];
-		} else if (breakPoints[sizes.mediumLarge] && breakPoints[sizes.mediumLarge] >= size.width) {
-			DisplayedComponent = components[sizes.mediumLarge];
-		} else if (breakPoints[sizes.large] >= size.width) {
-			DisplayedComponent = components[sizes.large];
-		} else if (breakPoints[sizes.xLarge] && breakPoints[sizes.xLarge] >= size.width) {
-			DisplayedComponent = components[sizes.xLarge];
-		}
+    if (componentBreakPoints[breakpoints.xSmall] && componentBreakPoints[breakpoints.xSmall] >= size.width) {
+      DisplayedComponent = components[breakpoints.xSmall]
+    } else if (componentBreakPoints[breakpoints.small] && componentBreakPoints[breakpoints.small] >= size.width) {
+      DisplayedComponent = components[breakpoints.small]
+    } else if (
+      componentBreakPoints[breakpoints.mediumSmall] &&
+      componentBreakPoints[breakpoints.mediumSmall] >= size.width
+    ) {
+      DisplayedComponent = components[breakpoints.mediumSmall]
+    } else if (componentBreakPoints[breakpoints.medium] && componentBreakPoints[breakpoints.medium] >= size.width) {
+      DisplayedComponent = components[breakpoints.medium]
+    } else if (
+      componentBreakPoints[breakpoints.mediumLarge] &&
+      componentBreakPoints[breakpoints.mediumLarge] >= size.width
+    ) {
+      DisplayedComponent = components[breakpoints.mediumLarge]
+    } else if (componentBreakPoints[breakpoints.large] >= size.width) {
+      DisplayedComponent = components[breakpoints.large]
+    } else if (componentBreakPoints[breakpoints.xLarge] && componentBreakPoints[breakpoints.xLarge] >= size.width) {
+      DisplayedComponent = components[breakpoints.xLarge]
+    }
 
-		return <DisplayedComponent {...props} />;
-	};
+    return <DisplayedComponent {...props} />
+  }
 
-	return <ResponsiveWrapper>{size => getSizedElement(size)}</ResponsiveWrapper>;
-};
+  return <ResponsiveWrapper>{size => getSizedElement(size)}</ResponsiveWrapper>
+}
 
 const withAdaptive = ({ mapSizeToComponent }) => props => {
-	return <ResponsiveWrapper>{size => mapSizeToComponent(size)(props)}</ResponsiveWrapper>;
-};
+  return <ResponsiveWrapper>{size => mapSizeToComponent(size)(props)}</ResponsiveWrapper>
+}
 
 const withSize = Component => props => (
-	<ResponsiveWrapper>{size => <Component size={size} {...props} />}</ResponsiveWrapper>
-);
+  <ResponsiveWrapper>{size => <Component size={size} {...props} />}</ResponsiveWrapper>
+)
 
-export { sizes, withBreakpoints, withAdaptive, withSize };
+export { breakpoints, withBreakpoints, withAdaptive, withSize }
